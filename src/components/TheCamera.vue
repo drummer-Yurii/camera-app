caf748289d9c328a303fd98bcfadb00119084e1e
Yurii Rybachok <drummer2009@ukr.net>
2023-11-17 09:59:30 +0200

    functionality for video & take picture

--- src/components/TheCamera.vue
+++ src/components/TheCamera.vue
@@ -1,5 +1,47 @@
 <script setup>
+import { ref, onMounted, warn } from 'vue';
 
+const canvas = ref(null);
+const video = ref(null);
+const ctx = ref(null);
+
+const constraints = ref({
+    audio: false,
+    video: true,
+});
+
+onMounted(async () => {
+    if (video.value && canvas.value) {
+        ctx.value = canvas.value.getContext("2d");
+
+        await navigator.mediaDevices
+            .getUserMedia(constraints.value)
+            .then(SetStream)
+            .catch(e => {
+                console.error(e);
+            })
+    }
+});
+
+function SetStream (stream) {
+    video.value.srcObject = stream;
+    video.value.play();
+
+    requestAnimationFrame(Draw);
+};
+
+function Draw () {
+    ctx.value.drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height);
+
+    requestAnimationFrame(Draw);
+};
+
+function TakePicture() {
+    var link = document.createElement('a');
+    link.download = `vue-cam-${new Date().toISOString()}.png`
+    link.href = canvas.value.toDataURL();
+    link.click();
+}
 </script>
 
 <template>
@@ -9,7 +51,10 @@
         <canvas ref="canvas" width="800" height="420" class="bg-black rounded-3xl"></canvas>
 
         <div class="flex items-center justify-center py-4">
-            <button class="px-6 py-4 bg-green-500 rounded text-white text-2xl uppercase font-bold hover:bg-green-600">
+            <button
+                @click="TakePicture" 
+                class="px-6 py-4 bg-green-500 rounded text-white text-2xl uppercase font-bold hover:bg-green-600"
+            >
                 Take Picture
             </button>
         </div>
